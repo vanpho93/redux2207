ce102a29112d55c8f9015297adbe7761ec2cc315
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-26 11:11:16 +0700

    coding

--- src/components/WordFilter.js
+++ src/components/WordFilter.js
@@ -6,15 +6,29 @@ class WordFilter extends Component {
         this.props.dispatch({ type: 'CHANGE_FILTER_STATUS', newStatus });
     }
 
+    
+
     render() {
+        const getStyle = (status) => {
+            const condition = this.props.filterStatus === status;
+            return condition ? { color: 'green' } : { color: 'black' };
+        }
         return (
             <div>
-                <button onClick={() => this.changeFilterStatus('SHOW_ALL')}>SHOW_ALL</button>            
-                <button onClick={() => this.changeFilterStatus('SHOW_MEMORIZED')}>SHOW_MEMORIZED</button>            
-                <button onClick={() => this.changeFilterStatus('SHOW_FORGOT')}>SHOW_FORGOT</button>  
+                <button onClick={() => this.changeFilterStatus('SHOW_ALL')} style={getStyle('SHOW_ALL')}>
+                    SHOW_ALL
+                </button>            
+                <button onClick={() => this.changeFilterStatus('SHOW_MEMORIZED')} style={getStyle('SHOW_MEMORIZED')}>
+                    SHOW_MEMORIZED
+                </button>            
+                <button onClick={() => this.changeFilterStatus('SHOW_FORGOT')} style={getStyle('SHOW_FORGOT')}>
+                    SHOW_FORGOT
+                </button>  
             </div>          
         );
     }
 }
 
-export default connect()(WordFilter);
+const mapStateToProp = state => ({ filterStatus: state.filterStatus });
+
+export default connect(mapStateToProp)(WordFilter);
