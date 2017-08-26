09b10f90d0a39fcacd6126733ee19f3aa2c1f131
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-26 14:25:18 +0700

    xong

--- src/components/WordFilter.js
+++ src/components/WordFilter.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import * as actionCreators from '../redux/action';
 
 class WordFilter extends Component {
-    changeFilterStatus(newStatus) {
-        this.props.dispatch({ type: 'CHANGE_FILTER_STATUS', newStatus });
-    }
-
     render() {
         const getStyle = (status) => {
             const condition = this.props.filterStatus === status;
             return condition ? { color: 'green' } : { color: 'black' };
         }
+        const { changeFilterStatus } = this.props;
         return (
             <div>
-                <button onClick={() => this.changeFilterStatus('SHOW_ALL')} style={getStyle('SHOW_ALL')}>
+                <button onClick={() => changeFilterStatus('SHOW_ALL')} style={getStyle('SHOW_ALL')}>
                     SHOW_ALL
                 </button>            
-                <button onClick={() => this.changeFilterStatus('SHOW_MEMORIZED')} style={getStyle('SHOW_MEMORIZED')}>
+                <button onClick={() => changeFilterStatus('SHOW_MEMORIZED')} style={getStyle('SHOW_MEMORIZED')}>
                     SHOW_MEMORIZED
                 </button>            
-                <button onClick={() => this.changeFilterStatus('SHOW_FORGOT')} style={getStyle('SHOW_FORGOT')}>
+                <button onClick={() => changeFilterStatus('SHOW_FORGOT')} style={getStyle('SHOW_FORGOT')}>
                     SHOW_FORGOT
                 </button>  
             </div>          
@@ -29,4 +27,4 @@ class WordFilter extends Component {
 
 const mapStateToProp = state => ({ filterStatus: state.filterStatus });
 
-export default connect(mapStateToProp)(WordFilter);
+export default connect(mapStateToProp, actionCreators)(WordFilter);
